f8b0b50029b6f21d226393b0ec174bcbadfb5da3
Daniel Bianchin <danielbianchin@outlook.com.br>
2022-11-25 15:43:06 -0300

    Há um par entre nós

---
+++ fundamentos/secao-04-introducao-a-js/js-dia-01-primeiros-passos/exercicios/Há um par entre nós.js
@@ -0,0 +1,23 @@
+// PAR
+
+// const n1 = 1;
+// const n2 = 5;
+// const n3 = 2;
+
+// if (n1 % 2 === 0 || n2 % 2 === 0 || n3 % 2 === 0) {
+//     console.log (true);
+// } else {
+//     console.log(false);
+// }
+
+//IMPAR
+
+const n1 = 4;
+const n2 = 1;
+const n3 = 2;
+
+if (n1 % 2 != 0 || n2 % 2 != 0 || n3 % 2 != 0) {
+    console.log (true);
+} else {
+    console.log(false);
+}
