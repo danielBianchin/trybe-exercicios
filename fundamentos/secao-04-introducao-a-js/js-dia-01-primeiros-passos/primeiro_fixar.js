9fdef7e37ba60803a5b23356a62157c7be98e5e2
Daniel Bianchin <danielbianchin@outlook.com.br>
2022-11-25 11:44:23 -0300

    Primeiro fixar completo

---
+++ fundamentos/secao-04-introducao-a-js/js-dia-01-primeiros-passos/primeiro_fixar.js
@@ -0,0 +1,14 @@
+const myname = "Daniel";
+const birthCity = "Chapecó";
+let birthYear = 2000;
+ 
+console.log(myname, birthCity, birthYear);
+
+birthYear = 2030;
+console.log(birthYear)
+
+birthCity = "Arvoredo";
+console.log(birthCity);
+
+
+// A mensagem de erro acontece pois não é permitido alterar variaveis const.
