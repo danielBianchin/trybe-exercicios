82ed098a6e22aa42523756c836ff079143b28c78
Daniel Bianchin <danielbianchin@outlook.com.br>
2022-11-28 18:11:12 -0300

    Bubble sort ordem crescente

---
+++ fundamentos/secao-04-introducao-a-js/js-dia-02-array-loopFor/Bubble Sort.js
@@ -0,0 +1,13 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let aux = 0;
+for (let index = 0; index < numbers.length; index += 1) {
+    for (let segundoIndex = 0; segundoIndex < numbers.length - index; segundoIndex += 1) {
+        if (numbers[segundoIndex] > numbers[segundoIndex + 1]) {
+            aux = numbers[segundoIndex];
+            numbers[segundoIndex] = numbers[segundoIndex + 1];
+            numbers[segundoIndex + 1] = aux;
+        }
+    }
+}
+
+console.log(numbers);
