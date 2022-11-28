87966ddd689707e7a8329244601acc038df1bd7f
Daniel Bianchin <danielbianchin@outlook.com.br>
2022-11-28 10:05:51 -0300

    Maior valor

--- fundamentos/secao-04-introducao-a-js/js-dia-02-array-loopFor/exercicio Lidando com Arrays.js
+++ fundamentos/secao-04-introducao-a-js/js-dia-02-array-loopFor/exercicio Lidando com Arrays.js
@@ -1,13 +1,17 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let soma = 0;
 let mediaAritmetica = 0;
+let maiorValor = 0;
 
 for (let number of numbers) {
-    console.log(number);
+    // console.log(number);
     soma = soma + number;
+    if (number > maiorValor) {
+        maiorValor = number;
+    }
 }
 
-console.log ("A soma de todos os valores contidos no array é: ", soma);
+console.log ("A soma de todos os valores contidos no array é:", soma);
 
 mediaAritmetica = soma / numbers.length;
 
@@ -16,3 +20,6 @@ if (mediaAritmetica > 20) {
 } else {
     console.log("Valor da media aritmética menor ou igual a 20");
 }
+
+console.log ("O maior valor do array é:", maiorValor);
+
