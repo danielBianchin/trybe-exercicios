4985b968b8d88b49fe9a3671a9e0db311688f098
Daniel Bianchin <danielbianchin@outlook.com.br>
2022-11-28 10:03:25 -0300

    Media aritmetica

--- fundamentos/secao-04-introducao-a-js/js-dia-02-array-loopFor/exercicio Lidando com Arrays.js
+++ fundamentos/secao-04-introducao-a-js/js-dia-02-array-loopFor/exercicio Lidando com Arrays.js
@@ -1,6 +1,18 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let soma = 0;
+let mediaAritmetica = 0;
 
 for (let number of numbers) {
     console.log(number);
+    soma = soma + number;
 }
 
+console.log ("A soma de todos os valores contidos no array é: ", soma);
+
+mediaAritmetica = soma / numbers.length;
+
+if (mediaAritmetica > 20) {
+    console.log("Valor da media aritmética maior que 20");
+} else {
+    console.log("Valor da media aritmética menor ou igual a 20");
+}
