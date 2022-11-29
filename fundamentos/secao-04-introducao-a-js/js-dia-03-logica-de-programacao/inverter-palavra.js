ab626dedd54cf167b38cf825b4f303bab8306d56
Daniel Bianchin <danielbianchin@outlook.com.br>
2022-11-29 11:13:42 -0300

    Algoritmo para Inverter palavras

--- fundamentos/secao-04-introducao-a-js/js-dia-03-logica-de-programacao/inverter-palavra.js
+++ fundamentos/secao-04-introducao-a-js/js-dia-03-logica-de-programacao/inverter-palavra.js
@@ -1,30 +1,37 @@
 //Tentei resolver dessa forma, no entanto parece que Strings não podem ter os indices reatribuidos, a ideia no entanto é bastante válida, visto que tentei utilizando arrays numéricos de qualquer tamanho, e funciona perfeitamente invertendo todos os números.
 
 // este não funciona
-let word = 'trybe';
+// let word = 'trybe';
 
-for (let index = 0; index <= word.lenght/2 - 1 ; index += 1) {
+// for (let index = 0; index <= word.lenght/2 - 1 ; index += 1) {
     
-    let aux = word[word.length - 1 - index];
-    word[word.length - 1 - index] = word[index]; 
-    word[index] = aux;
+//     let aux = word[word.length - 1 - index];
+//     word[word.length - 1 - index] = word[index]; 
+//     word[index] = aux;
     
-}
-console.log(word);
+// }
+// console.log(word);
 
 // este funciona
-let array = [20,19,18,17,16,15,14,13,12,11,10,9,8,7,6,5,4,3,2,1];
+// let array = [20,19,18,17,16,15,14,13,12,11,10,9,8,7,6,5,4,3,2,1];
 
-for (let index = 0; index <= array.length/2 - 1 ; index += 1) {
+// for (let index = 0; index <= array.length/2 - 1 ; index += 1) {
     
-    let aux = array[array.length - 1 - index];
-    array[array.length - 1 - index] = array[index]; 
-    array[index] = aux;
+//     let aux = array[array.length - 1 - index];
+//     array[array.length - 1 - index] = array[index]; 
+//     array[index] = aux;
     
+// }
+// console.log(array);
+
+let word = 'tryber';
+let invertedWord = '';
+
+for (index = word.length - 1; index >= 0; index -= 1) {
+    invertedWord = invertedWord + (word[index])
 }
-console.log(array);
 
-
+console.log(invertedWord);
 
 
 
