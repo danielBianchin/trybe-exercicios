fb90c42a60527557be0293ef803af56590fb0fd2
Daniel Bianchin <danielbianchin@outlook.com.br>
2022-11-29 15:38:21 -0300

    quadrado de asteriscos de n lados

---
+++ fundamentos/secao-04-introducao-a-js/js-dia-03-logica-de-programacao/exercicio-bonus.js
@@ -0,0 +1,10 @@
+let n = 9;
+let asterisco = '';
+
+for (index = 0; index < n; index += 1) {
+    asterisco = asterisco + '*'
+}
+
+for (index = 0; index < n; index += 1) {
+    console.log(asterisco);
+}
