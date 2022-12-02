968209d196cdf1a05e310095808a090e49343103
Daniel Bianchin <danielbianchin@outlook.com.br>
2022-12-02 10:34:05 -0300

    Duas funções que retornam o indice do maior e menor valor de um array

--- fundamentos/secao-04-introducao-a-js/js-dia-04-obj-e-funcoes/ex-funções/ex-array-maiorvalor.js
+++ fundamentos/secao-04-introducao-a-js/js-dia-04-obj-e-funcoes/ex-funções/ex-array-maiorvalor.js
@@ -1,4 +1,5 @@
 function arrayMaiorValorIndice (array) {
+    let maiorValorIndice
     let maiorValor = array[0];
     for (index in array) {
         if (array[index] > maiorValor) {
@@ -12,4 +13,17 @@ let array = [2,3,6,7,10,1];
 
 console.log (arrayMaiorValorIndice(array))
 
+function arrayMenorValorIndice (array) {
+    let menorValorIndice = 0
+    let menorValor = array[0];
+    for (index in array) {
+        if (array[index] < menorValor) {
+            menorValorIndice = index;
+        }
+    }
+    return menorValorIndice;
+}
 
+let array2 = [2, 4, 6, 7, 10, 0, -3];
+
+console.log(arrayMenorValorIndice(array2));
